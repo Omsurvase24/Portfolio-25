3d3c9b9b94a9fc6a7f7acd216b7ccc185ce79708
Omsurvase24 <omsurvase24@gmail.com>
2024-09-26 16:57:52 +0530

    transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,39 @@
 
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [currText, setCurrText] = useState("Freelancer");
+  const texts = ["Freelancer", "Backend Developer", "Full Stack Developer"];
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFade(false);
+      setTimeout(() => {
+        setCurrText((prevText) => {
+          const currIndex = texts.indexOf(prevText);
+          const nextIndex = (currIndex + 1) % texts.length;
+          return texts[nextIndex];
+        });
+        setFade(true);
+      }, 500);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto p-2">
         <div className="flex items-center justify-between ml-20 mr-20">
-          <div>
-            <h1 className="text-3xl font-bold text-orange-500">
-              Welcome to My Portfolio
+          <div className={`transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}>
+            <p>Hi, I am Om Survase.</p>
+            <h1 className="text-5xl font-bold text-orange-500">
+              {currText}
             </h1>
-            <p>This is the homepage content.</p>
           </div>
           <div>
             <Image src="/b&w.webp" alt="Portfolio Image" width={300} height={300} />
