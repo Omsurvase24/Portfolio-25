5a4104bc41cc771acd2efebcc032d19437590a69
Omsurvase24 <omsurvase24@gmail.com>
2024-09-27 00:32:52 +0530

    about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,14 +42,28 @@ export default function Home() {
             <Button radius="full" className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5 ml-2">
               Contact Info
             </Button>
-            <a href="http://github.com/Omsurvase24"><img src="/github.png" alt="github" width={40} className="fill-orange-500 mt-2" /></a>
-            <a href="https://www.linkedin.com/in/om-survase-812747241/"><img src="/linkedin.png" alt="github" width={40} className="fill-orange-500 mt-2" /></a>
-            <a href="https://leetcode.com/omsurvase24/"><img src="/leetcode.png" alt="github" width={40} className="fill-orange-500 mt-2" /></a>
+            <a href="http://github.com/Omsurvase24"><img src="/github.png" alt="github" width={40} className="mt-2" /></a>
+            <a href="https://www.linkedin.com/in/om-survase-812747241/"><img src="/linkedin.png" alt="github" width={40} className="mt-2" /></a>
+            <a href="https://leetcode.com/omsurvase24/"><img src="/leetcode.png" alt="github" width={40} className="mt-2" /></a>
           </div>
           <div>
             <Image src="/b&w.webp" alt="Portfolio Image" width={300} height={300} />
           </div>
         </div>
+      </div>
+      <div className="container mx-auto p-10">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-5">About Me</h1>
+          <div className="flex justify-center">
+            <img src="https://user-images.githubusercontent.com/10498744/210012254-234538ff-d198-48aa-8964-37e6fd45d227.gif" alt="mario" width={400} />
+          </div>
+          <p className="mt-5">
+            Hello, I am a passionate backend developer with penchant for Python and Django. What sets me apart from my batchmates is that along with my technical expertise, I also have a foundation in leading teams and volunteering for events. I have worked as a data science intern for a few months adding to my interest for Python. I am very curious as a person so you will always find me exploring something, may it be a new tech stack or places. I believe that collaboration and continuous learning are the cornerstones of growth, and I'm always eager to connect with like-minded individuals who share my passion for innovation.
+          </p>
+        </div>
+      </div>
+      <div className="container mx-auto p-10 text-center">
+        <h1 className="text-5xl font-bold mb-5">Skills</h1>
       </div>
     </>
   );
