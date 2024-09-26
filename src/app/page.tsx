ed1fe1ab7f15f78c9f769f1ca5b5d696eee1c7b1
Omsurvase24 <omsurvase24@gmail.com>
2024-09-26 23:32:43 +0530

    links and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import { Button } from "@nextui-org/button";
 import { useState, useEffect } from "react";
 
 export default function Home() {
@@ -30,11 +31,20 @@ export default function Home() {
       <Navbar />
       <div className="container mx-auto p-2">
         <div className="flex items-center justify-between ml-20 mr-20">
-          <div className={`transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}>
+          <div>
             <p>Hi, I am Om Survase.</p>
-            <h1 className="text-5xl font-bold text-orange-500">
+            <h1 className={`transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'} text-5xl font-bold text-orange-500`}>
               {currText}
             </h1>
+            <Button radius="full" className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5">
+              Resume
+            </Button>
+            <Button radius="full" className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5 ml-2">
+              Contact Info
+            </Button>
+            <a href="http://github.com/Omsurvase24"><img src="/github.png" alt="github" width={40} className="fill-orange-500 mt-2" /></a>
+            <a href="https://www.linkedin.com/in/om-survase-812747241/"><img src="/linkedin.png" alt="github" width={40} className="fill-orange-500 mt-2" /></a>
+            <a href="https://leetcode.com/omsurvase24/"><img src="/leetcode.png" alt="github" width={40} className="fill-orange-500 mt-2" /></a>
           </div>
           <div>
             <Image src="/b&w.webp" alt="Portfolio Image" width={300} height={300} />
