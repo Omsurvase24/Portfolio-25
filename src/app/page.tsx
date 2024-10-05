52ed5c9f6e47026a66d1fef8ef1f1c32317fe062
Omsurvase24 <omsurvase24@gmail.com>
2024-10-05 20:51:26 +0530

    resume and contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ import Navbar from "@/components/Navbar";
 import { Button } from "@nextui-org/button";
 import { useState, useEffect } from "react";
 
+const handleOpenPDF = () => {
+  window.open('/resume.pdf', '_blank');
+};
+
+const handleSendEmail = () => {
+  window.location.href = 'mailto:omsurvase24@gmail.com';
+};
+
 export default function Home() {
   const [currText, setCurrText] = useState("Freelancer");
   const texts = ["Freelancer", "Backend Developer", "Full Stack Developer"];
@@ -36,11 +44,11 @@ export default function Home() {
             <h1 className={`transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'} text-5xl font-bold text-orange-500`}>
               {currText}
             </h1>
-            <Button radius="full" className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5 font-bold">
+            <Button radius="full" onClick={handleOpenPDF} className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5 font-bold">
               Resume
             </Button>
-            <Button radius="full" className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5 ml-2 font-bold">
-              Contact Info
+            <Button radius="full" onClick={handleSendEmail} className="bg-gradient-to-tr from-orange-300 to-orange-500 text-black shadow-lg mt-5 ml-2 font-bold">
+              Contact Me
             </Button>
             <a href="http://github.com/Omsurvase24"><img src="/github.png" alt="github" width={40} className="mt-2" /></a>
             <a href="https://www.linkedin.com/in/om-survase-812747241/"><img src="/linkedin.png" alt="github" width={40} className="mt-2" /></a>
